18a15249be3e4ea3e940652cd041d2d4660b423a
Peter <petergorman11@gmail.com>
2020-01-28 15:35:44 -0500

    V2.0

---
+++ gamemodes/starwarsrp/gamemode/shared.lua
@@ -0,0 +1,10 @@
+GM.Name = "StarWarsRP"
+GM.Author = "[TW] gunman435"
+GM.Email = "tamewater.gunman@gmail.com"
+GM.Website = "www.tamewater.com"
+
+DeriveGamemode( "sandbox" )
+
+function GM:Initialize()
+	print("This isn't a real gamemode lol")
+end
